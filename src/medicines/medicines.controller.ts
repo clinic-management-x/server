1f144688f023268e15dd9fbbf40de048d864c1e9
rachel <shwehnit14@gmail.com>
2024-09-11 00:08:42 +0630

    feat: barcode create

--- src/medicines/medicines.controller.ts
+++ src/medicines/medicines.controller.ts
@@ -82,7 +82,7 @@ export class MedicinesController {
         );
     }
 
-    @Post("/barcode")
+    @Post("/barcodes")
     async createBarcode(
         @Body() createBarcodeDto: CreateBarcodeDto[],
         @Request() request: ClinicRequest
