62ce455605769a99015363ea022c0002bc1c67de
rachel <shwehnit14@gmail.com>
2024-10-24 23:05:13 +0630

    fix: add filter

---
+++ src/appointments/dtos/get-appointments.dto.ts
@@ -0,0 +1,50 @@
+import { Type } from "class-transformer";
+import {
+    IsDateString,
+    IsInt,
+    IsOptional,
+    IsString,
+    Max,
+    MaxLength,
+    Min,
+} from "class-validator";
+
+export class GetAppointmentsDto {
+    @IsOptional()
+    @Type(() => Number)
+    @IsInt()
+    @Min(0)
+    @Max(10)
+    limit?: number = 10;
+
+    @IsOptional()
+    @Type(() => Number)
+    @IsInt()
+    @Min(0)
+    skip?: number = 0;
+
+    @IsOptional()
+    @IsString()
+    @MaxLength(50)
+    search?: string;
+
+    @IsOptional()
+    @IsString()
+    nearest?: string;
+
+    @IsOptional()
+    @IsString()
+    necessity?: string;
+
+    @IsOptional()
+    @IsString()
+    status?: string;
+
+    @IsOptional()
+    @IsDateString()
+    start?: string;
+
+    @IsOptional()
+    @IsDateString()
+    end?: string;
+}
